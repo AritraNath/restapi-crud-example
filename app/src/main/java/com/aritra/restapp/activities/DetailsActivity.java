38217feb2bb5e1ceec0a39959cdac359cdf29dba
Aritra <aritranath.online@gmail.com>
2019-06-17 23:19:40 +0530

    third commit

--- app/src/main/java/com/aritra/restapp/activities/DetailsActivity.java
+++ app/src/main/java/com/aritra/restapp/activities/DetailsActivity.java
@@ -1,6 +1,7 @@
 package com.aritra.restapp.activities;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,10 +25,14 @@ import com.aritra.restapp.data.Details;
 import com.aritra.restapp.R;
 import com.aritra.restapp.util.Constants;
 import com.google.android.material.appbar.SubtitleCollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -36,11 +42,12 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     private TextView tvEduOrg, tvEduDegree, tvEduLoc, tvEduStartYear, tvEduEndYear;
     private TextView tvProOrg, tvProDesignation, tvProStartDate, tvProEndDate;
     RequestQueue requestQueue;
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog alertDialog;
+
     int id;
     String email, personalURL, educationURL, professionURL;
-    String perName, perLoc, perMob, perSkill, perLink;
-    String eduOrg, eduDegree, eduLoc, eduStartYear, eduEndYear;
-    String proOrg, proDesignation, proStartDate, proEndDate;
 
     Details details;
 
@@ -97,6 +104,9 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         ImageButton ivproDelete = findViewById(R.id.professionDeleteButton);
         ivproEdit.setOnClickListener(this);
         ivproDelete.setOnClickListener(this);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.fabLogout);
+        floatingActionButton.setOnClickListener(this);
 
         details = new Details();
         requestQueue = Volley.newRequestQueue(this);
@@ -182,28 +192,6 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
             }
         });
         requestQueue.add(requestPro);
-
-        getDataFromFields();
-    }
-
-    private void getDataFromFields() {
-        perName = Objects.requireNonNull(scLayout.getTitle()).toString();
-
-        perMob = tvPerMobile.getText().toString();
-        perLoc = tvPerLocation.getText().toString();
-        perSkill = tvPerSkills.getText().toString();
-        perLink = tvPerLinks.getText().toString();
-
-        eduOrg = tvEduOrg.getText().toString();
-        eduDegree = tvEduDegree.getText().toString();
-        eduLoc = tvEduLoc.getText().toString();
-        eduStartYear = tvEduStartYear.getText().toString();
-        eduEndYear = tvEduEndYear.getText().toString();
-
-        proOrg = tvProOrg.getText().toString();
-        proDesignation = tvProDesignation.getText().toString();
-        proStartDate = tvProStartDate.getText().toString();
-        proEndDate = tvProEndDate.getText().toString();
     }
 
     @Override
@@ -251,41 +239,238 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     private void updatePersonalDetails() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup_personal, null);
+        final TextInputEditText personalDetailName = view.findViewById(R.id.personalName);
+        final TextInputEditText personalDetailLocation = view.findViewById(R.id.personalLocation);
+        final TextInputEditText personalDetailPhone = view.findViewById(R.id.personalPhone);
+        final TextInputEditText personalDetailSkills = view.findViewById(R.id.personalSkill);
+        final TextInputEditText personalDetailLinks = view.findViewById(R.id.personalLinks);
 
+        personalDetailName.setText(scLayout.getTitle());
+        personalDetailLocation.setText(tvPerLocation.getText());
+        personalDetailPhone.setText(tvPerMobile.getText());
+        personalDetailSkills.setText(tvPerSkills.getText());
+        personalDetailLinks.setText(tvPerLinks.getText());
+
+        Button saveButton = view.findViewById(R.id.personalSave);
+        saveButton.setText("Update");
+
+        dialogBuilder.setView(view);
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String skills = personalDetailSkills.getText().toString();
+                String mobile_no = personalDetailPhone.getText().toString();
+                String name = personalDetailName.getText().toString();
+                String links = personalDetailLinks.getText().toString();
+                String location = personalDetailLocation.getText().toString();
+
+                Map<String, String> personalParams = new HashMap<>();
+                personalParams.put("skills", skills);
+                personalParams.put("mobile_no", mobile_no);
+                personalParams.put("name", name);
+                personalParams.put("links", links);
+                personalParams.put("location", location);
+                personalParams.put("email", email);
+
+                JSONObject personalDetails = new JSONObject(personalParams);
+
+                JsonObjectRequest personalRequest = new JsonObjectRequest(Request.Method.PUT,
+                        Constants.BASE_URL + Constants.PERSONAL_DETAILS_URL + id, personalDetails, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Toast.makeText(DetailsActivity.this, "Personal details updated", Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                requestQueue.add(personalRequest);
+                alertDialog.dismiss();
+                startActivity(new Intent(DetailsActivity.this, DetailsActivity.class)
+                        .putExtra("userID", id)
+                        .putExtra("email", email));
+
+            }
+        });
     }
 
     private void updateEducationDetails() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup_educational, null);
 
+        final TextInputEditText educationStartYear = view.findViewById(R.id.educationDetailStartYear);
+        final TextInputEditText educationEndYear = view.findViewById(R.id.educationDetailEndYear);
+        final TextInputEditText educationDegree = view.findViewById(R.id.educationDetailDegree);
+        final TextInputEditText educationOrganisation = view.findViewById(R.id.educationDetailOrganisation);
+        final TextInputEditText educationLocation = view.findViewById(R.id.educationDetailLocation);
+        Button saveButton = view.findViewById(R.id.educationalSave);
+        saveButton.setText("Update");
+
+        educationStartYear.setText(tvEduStartYear.getText());
+        educationEndYear.setText(tvEduEndYear.getText());
+        educationDegree.setText(tvEduDegree.getText());
+        educationOrganisation.setText(tvEduOrg.getText());
+        educationLocation.setText(tvEduLoc.getText());
+
+        dialogBuilder.setView(view);
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String start_year = educationStartYear.getText().toString();
+                String degree = educationDegree.getText().toString();
+                String organisation = educationOrganisation.getText().toString();
+                String location = educationLocation.getText().toString();
+                String end_year = educationEndYear.getText().toString();
+
+                Map<String, String> educationParams = new HashMap<>();
+                educationParams.put("start_year", start_year);
+                educationParams.put("degree", degree);
+                educationParams.put("organisation", organisation);
+                educationParams.put("location", location);
+                educationParams.put("end_year", end_year);
+
+                JSONObject educationObject = new JSONObject(educationParams);
+
+                JsonObjectRequest educationRequest = new JsonObjectRequest(Request.Method.PUT,
+                        Constants.BASE_URL + Constants.EDUCATION_DETAILS_URL + id, educationObject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(DetailsActivity.this, "Educational details updated", Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                requestQueue.add(educationRequest);
+                alertDialog.dismiss();
+                startActivity(new Intent(DetailsActivity.this, DetailsActivity.class)
+                .putExtra("userID", id)
+                .putExtra("email", email));
+            }
+        });
     }
 
     private void updateProfessionalDetails() {
+        dialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.popup_proffesional, null);
 
+        final TextInputEditText professionStartDate = view.findViewById(R.id.professionalStartDate);
+        final TextInputEditText professionEndDate = view.findViewById(R.id.professionalEndDate);
+        final TextInputEditText professionOrganisation = view.findViewById(R.id.professionalOrganisation);
+        final TextInputEditText professionDesignation = view.findViewById(R.id.professionalDesignation);
+        Button saveButton = view.findViewById(R.id.professionalSave);
+        saveButton.setText("Update");
+
+        professionStartDate.setText(tvProStartDate.getText());
+        professionEndDate.setText(tvProEndDate.getText());
+        professionOrganisation.setText(tvProOrg.getText());
+        professionDesignation.setText(tvProDesignation.getText());
+
+        dialogBuilder.setView(view);
+        alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String end_date = professionEndDate.getText().toString();
+                String organisation = professionOrganisation.getText().toString();
+                String designation = professionDesignation.getText().toString();
+                String start_date = professionStartDate.getText().toString();
+
+                Map<String, String> professionalParams = new HashMap<>();
+                professionalParams.put("end_date", end_date);
+                professionalParams.put("organisation", organisation);
+                professionalParams.put("designation", designation);
+                professionalParams.put("start_date", start_date);
+
+                JSONObject educationObject = new JSONObject(professionalParams);
+                JsonObjectRequest professionalRequest = new JsonObjectRequest(Request.Method.PUT,
+                        Constants.BASE_URL + Constants.PROFESSIONAL_DETAILS_URL + id, educationObject,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Toast.makeText(DetailsActivity.this, "Professional details updated",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+                requestQueue.add(professionalRequest);
+                alertDialog.dismiss();
+                startActivity(new Intent(DetailsActivity.this, DetailsActivity.class)
+                        .putExtra("userID", id)
+                        .putExtra("email", email));
+            }
+        });
     }
 
     private void deletePersonalDetails() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.DELETE, personalURL,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(request);
     }
 
     private void deleteEducationDetails() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.DELETE, educationURL,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(request);
     }
 
     private void deleteProfessionalDetails() {
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.DELETE, professionURL,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
 
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(request);
     }
 
     private void logoutUser() {
         startActivity(new Intent(DetailsActivity.this, LoginActivity.class));
     }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            finish();
-            startActivity(new Intent(DetailsActivity.this, DetailsActivity.class)
-            .putExtra("userID", id)
-            .putExtra("email", email));
-        }
-    }
 }
